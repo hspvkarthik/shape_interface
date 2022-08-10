f60883826430e958255be65c90db1cfbaa0b7903
hspvkarthik <hspvkarthik@shopup.org>
2022-08-10 22:33:24 +0530

    Add rectangle area and perimeter definition

--- rect.go
+++ rect.go
@@ -1,5 +1,7 @@
 package shape_interface
 
+import "math"
+
 type Rectangle struct {
 	length float64
 	width  float64
@@ -10,4 +12,28 @@ func NewRectangle(length float64, width float64) Rectangle {
 		panic("length or width should be positive")
 	}
 	return Rectangle{length: length, width: width}
+}
+
+func (rect Rectangle) FindPerimeter() float64 {
+	if rect.length == 1 {
+		return 2 + 2*rect.width
+	} else if rect.width == 1 {
+		return 2 + 2*rect.length
+	} else if rect.length == rect.width {
+		return rect.length * 4
+	} else {
+		return 2 * (rect.length + rect.width)
+	}
+}
+
+func (rect Rectangle) FindArea() float64 {
+	if rect.length == 1 {
+		return rect.width
+	} else if rect.width == 1 {
+		return rect.length
+	} else if rect.length == rect.width {
+		return math.Pow(rect.length, 2)
+	} else {
+		return rect.length * rect.width
+	}
 }
